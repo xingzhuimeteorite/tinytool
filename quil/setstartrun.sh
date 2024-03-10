300a65f8e6c4b4454447941cdb9188eaf19e81a6
sol <1129514580@qq.com>
2024-03-11 03:22:22 +0800

    关闭死亡会话

--- quil/setstartrun.sh
+++ quil/setstartrun.sh
@@ -2,6 +2,7 @@ cd  /etc/systemd/system
 echo '#!/bin/bash' > start_quil.sh
 echo '' >> start_quil.sh
 echo '# 检查是否存在 screen 会话' >> start_quil.sh
+echo 'screen -wipe' >> start_quil.sh
 echo 'if screen -list | grep -q "node"; then' >> start_quil.sh
 echo '  echo "Screen session '\''node'\'' already exists."' >> start_quil.sh
 echo '  exit 0' >> start_quil.sh
